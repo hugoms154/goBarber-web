b5538e5a2a4dc0650df5a2f5d379bd8c320091c4
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-06 16:15:48 -0300

    use routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
@@ -9,10 +9,13 @@ import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AppProvider>
-      <SignIn />
-    </AppProvider>
-    <GlobalStyle />
+    <Router>
+      <AppProvider>
+        <Routes />
+      </AppProvider>
+
+      <GlobalStyle />
+    </Router>
   </>
 );
 
